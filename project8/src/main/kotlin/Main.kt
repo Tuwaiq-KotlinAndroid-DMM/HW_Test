c1ea8ec35e53c99f62781482f8df91f8b744cc27
Your Name <amal-alshahrani20@hotmail.com>
2021-09-28 16:44:05 +0300

    homework

---
+++ project8/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var manth = readLine()?.toInt()
+    if ( manth==1){
+        println(31)
+    }
+    var manth = readLine()?.toInt()
+    if (manth==2)
+        println(28)
+
+}
